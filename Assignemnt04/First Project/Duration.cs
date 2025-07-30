5250459dc42cb0566bbe9cafc2dfd81f232d0958
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-07-30 20:10:15 +0300

    First Project

---
+++ Assignemnt04/First Project/Duration.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignemnt04.First_Project
+{
+    internal class Duration
+    {
+        private int _hours;
+        private int _minutes;
+        private int _seconds;
+
+
+        public int Hours
+        {
+            get { return _hours; }
+            set { _hours = value; }
+        }
+
+        public int Minutes 
+        {
+            get { return _minutes; }
+            set
+            { 
+                _minutes = value % 60;
+                Hours += value / 60;
+
+                 if(_minutes < 0)
+                {
+                    Hours--;
+                    Minutes += 60;
+                }
+            }
+        }
+        public int Seconds 
+        {
+            get { return _seconds; }
+            set 
+            {
+                _seconds = value%60;
+                Minutes = value/60;
+
+                if (_seconds < 0)
+                {
+                    Minutes--;
+                    Seconds += 60;
+                }
+            }
+        }
+           
+
+        public Duration(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        public Duration(int seconds)
+        {
+            Seconds = seconds % 60;
+            seconds -= Seconds;
+
+            Minutes = (seconds/60) % 60;
+            seconds-=Minutes*60;
+
+            Hours = seconds / 3600;
+        }
+
+        public override string ToString()
+        {
+            return $"Hours: {Hours}, Minutes: {Minutes}, Seconds: {Seconds}";
+        }
+
+        public static Duration operator + (Duration D1, Duration D2)
+        {
+            return new Duration(D1.Hours + D2.Hours, D1.Minutes + D2.Minutes, D1.Seconds + D2.Seconds);
+        }
+
+        public static Duration operator - (Duration D1, Duration D2)
+        {
+            return new Duration(D1.Hours - D2.Hours, D1.Minutes - D2.Minutes, D1.Seconds - D2.Seconds);
+        }
+
+
+        public static Duration operator + (Duration D1, int seconds)
+        {
+            return new Duration(D1.Hours, D1.Minutes, D1.Seconds + seconds);
+        }
+
+        public static Duration operator + (int seconds, Duration D1)
+        {
+            return new Duration(D1.Hours, D1.Minutes , D1.Seconds + seconds);
+        }
+
+        public static Duration operator - (int seconds, Duration D1)
+        {
+            return new Duration(D1.Hours, D1.Minutes, D1.Seconds - seconds);
+        }
+
+        public static Duration operator ++ (Duration D)
+        {
+            D.Minutes++;
+            return D;
+        }
+
+        public static Duration operator -- (Duration D)
+        {
+            D.Minutes--;
+            return D;
+        }
+
+        public static bool operator > (Duration D1, Duration D2)
+        {
+           if(D1.Hours == D2.Hours)
+           {
+                if (D1.Minutes == D2.Minutes)
+                {
+                    return D1.Seconds > D2.Seconds;
+                }
+
+                else
+                {
+                    return D1.Minutes > D2.Minutes;
+                }
+           }
+            
+            else
+            {
+                return D1.Hours > D2.Hours;
+            }
+        }
+
+        public static bool operator < (Duration D1, Duration D2)
+        {
+            if (D1.Hours == D2.Hours)
+            {
+                if (D1.Minutes == D2.Minutes)
+                {
+                    return D1.Seconds < D2.Seconds;
+                }
+
+                else
+                {
+                    return D1.Minutes < D2.Minutes;
+                }
+            }
+
+            else
+            {
+                return D1.Hours < D2.Hours;
+            }
+        }
+    }
+}
--- Assignemnt04/Program.cs
+++ Assignemnt04/Program.cs
@@ -1,10 +1,78 @@
-﻿namespace Assignemnt04
+﻿using Assignemnt04.First_Project;
+
+namespace Assignemnt04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Project01
+            /* 1. Define Class Duration To include Three Attributes Hours, Minutes and Seconds.
+             *
+             * 2. Define All Required Constructors to Produce this output:
+             * Duration D1 =new  Duration (1,10,15);
+             * D1.ToString();
+             * Output: Hours: 1, Minutes :10, Seconds :15
+             * Duration D1 =new Duration (3600);
+             * D1.ToString();
+             * Output: Hours: 1, Minutes :0, Seconds :0
+             * Duration D2 =new Duration (7800);
+             * D2.ToString();
+             * Output: Hours: 2, Minutes :10, Seconds :0
+             * Duration D3 =new Duration (666);
+             * D3.ToString();
+             * Output: Minutes :11, Seconds :6
+             * 
+             * 3. Implement All required Operators overloading to enable this Code:
+             *    D3=D1+D2
+             *    D3=D1 + 7800
+             *    D3=666+D3
+             *    D3= ++D1 (Increase One Minute)
+             *    D3 = --D2 (Decrease One Minute)
+             *    D1= D1 -D2
+             *    If (D1>D2) */
+
+            Duration D1 = new Duration(1, 10, 15);
+            Console.WriteLine(D1.ToString()); //Hours: 1, Minutes: 10, Seconds: 15
+
+            Duration D2 = new Duration(3600);
+            Console.WriteLine(D2.ToString()); //Hours: 1, Minutes: 0, Seconds: 0
+
+            Duration D3 = new Duration(7800);
+            Console.WriteLine(D3.ToString()); //Hours: 2, Minutes: 10, Seconds: 0
+
+            Duration D4 = new Duration(666);
+            Console.WriteLine(D4.ToString()); //Hours: 0, Minutes: 11, Seconds: 6
+
+            Console.WriteLine("===========================================================================================");
+
+            Console.WriteLine(D1+D2); //Hours: 2, Minutes: 0, Seconds: 15
+            Console.WriteLine(D1+7800); //Hours: 3, Minutes: 10, Seconds: 15
+            Console.WriteLine(666+D3); //Hours: 2, Minutes: 11, Seconds: 6
+
+            Console.WriteLine("===========================================================================================");
+
+            D3 = ++D1;
+            Console.WriteLine(D1); //Hours: 1, Minutes: 1, Seconds: 15
+
+            D3 = --D1;
+            Console.WriteLine(D1);
+
+            D1 = D1 - D2;
+            Console.WriteLine(D1);
+
+            if (D1>D2)
+            {
+                Console.WriteLine("D1 is greater than D2");
+            }
+
+            else
+            {
+                Console.WriteLine("D1 is not greater than D2");
+            }
+
+            #endregion
         }
+
     }
 }
